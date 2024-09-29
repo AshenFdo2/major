c404638dcfd6d663ead087a1d9ad9ab91a7da777
Savanop121 <160380281+Savanop121@users.noreply.github.com>
2024-09-29 20:30:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-  useTelegramBot: true, // Set to false if you don't want to use the Telegram bot
+  useTelegramBot: false, // Set to false if you don't want to use the Telegram bot
   telegramBotToken: process.env.TELEGRAM_BOT_TOKEN || "",
   authorizedUsers: process.env.AUTHORIZED_USERS
     ? process.env.AUTHORIZED_USERS.split(",").map((id) => parseInt(id.trim()))
